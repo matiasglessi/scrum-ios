eabbe8794948f270b66d4a2e6af2ffe13f81227f
Matias Glessi <matiasglessi@gmail.com>
2017-03-22 23:58:47 -0300

    add circular progress pod and sublevel xib

---
+++ scrum-ios/scrum-ios/SubLevelCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SubLevelCollectionViewCell.swift
+//  scrum-ios
+//
+//  Created by Matias Glessi on 3/22/17.
+//  Copyright © 2017 Matias Glessi. All rights reserved.
+//
+
+import UIKit
+
+class SubLevelCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
